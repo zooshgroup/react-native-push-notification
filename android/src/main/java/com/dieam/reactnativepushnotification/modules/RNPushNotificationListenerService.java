a14c70731d01808cb85b4698294f0f50e53cccef
Leendert van Beelen <leendert.van.beelen@zooshgroup.com>
2019-07-26 13:23:57 +0200

    Use FCM methods to receive token

--- android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotificationListenerService.java
+++ android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotificationListenerService.java
@@ -7,6 +7,7 @@ import com.google.firebase.messaging.RemoteMessage;
 import android.app.ActivityManager;
 import android.app.ActivityManager.RunningAppProcessInfo;
 import android.app.Application;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -26,6 +27,14 @@ import java.util.Random;
 import static com.dieam.reactnativepushnotification.modules.RNPushNotification.LOG_TAG;
 
 public class RNPushNotificationListenerService extends FirebaseMessagingService {
+
+    @Override
+    public void onNewToken(String token) {
+        Log.v(LOG_TAG, "onNewToken: " + token);
+        Intent intent = new Intent(this.getPackageName() + ".RNPushNotificationRegisteredToken");
+        intent.putExtra("token", token);
+        sendBroadcast(intent);
+    }
 
     @Override
     public void onMessageReceived(RemoteMessage message) {
--- android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotificationRegistrationService.java
+++ android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotificationRegistrationService.java
@@ -4,8 +4,9 @@ import android.app.IntentService;
 import android.content.Intent;
 import android.util.Log;
 
-import com.google.android.gms.gcm.GoogleCloudMessaging;
-import com.google.android.gms.iid.InstanceID;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 
 import static com.dieam.reactnativepushnotification.modules.RNPushNotification.LOG_TAG;
 
@@ -20,11 +21,13 @@ public class RNPushNotificationRegistrationService extends IntentService {
     @Override
     protected void onHandleIntent(Intent intent) {
         try {
-            String SenderID = intent.getStringExtra("senderID");
-            InstanceID instanceID = InstanceID.getInstance(this);
-            String token = instanceID.getToken(SenderID,
-                    GoogleCloudMessaging.INSTANCE_ID_SCOPE, null);
-            sendRegistrationToken(token);
+            FirebaseInstanceId.getInstance().getInstanceId().addOnSuccessListener(new OnSuccessListener<InstanceIdResult>() {
+                @Override
+                public void onSuccess(InstanceIdResult instanceIdResult) {
+                    String token = instanceIdResult.getToken();
+                    sendRegistrationToken(token);
+                }
+            });
         } catch (Exception e) {
             Log.e(LOG_TAG, TAG + " failed to process intent " + intent, e);
         }
